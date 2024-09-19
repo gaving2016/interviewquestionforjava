ac9a57a98b380c3c528e70c0425618811369fe06
gaozy6 <gaozy6@yusys.com.cn>
2024-09-19 15:49:37 +0800

    modify vuepress

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -5,7 +5,13 @@ import { defaultTheme } from '@vuepress/theme-default'
 export default defineUserConfig({
     title: 'My VuePress Site',
     description: 'This is my VuePress site',
-
+    head: [
+        ['link',{rel:'shortcut icon',href:'/imgs/home/credit.jpg'}],
+        ['meta', { name: 'theme-color', content: '#3eaf7c' }],
+        ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
+        ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }]
+    ],
+    base:'/interviewquestionforjava/',
     theme: defaultTheme({
         navbar: [
             { text: 'Home', link: '/' },
